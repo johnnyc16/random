4aa5dcbb02ec99ae52e9c3cbcba4f6d4373fb288
johnnyc16 <johnnyc16@gmail.com>
2015-07-10 00:54:49 +0200

    Nuevo commit
    Primer commit

---
+++ random.js
@@ -0,0 +1,8 @@
+//Math.Random() devuelve número aleatorio entre 0 y 1
+var numero = Math.Random();
+if (numero >= 0.5){
+  console.log('\n' + numero + ' MENOR QUE 0.5');
+}
+else {
+  console.log('\n' + numero + ' MAYOR QUE 0.5');  
+}
